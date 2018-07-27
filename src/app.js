d228062c7c9fcfa170d7e0151ec64f507a75ffec
Louis <michael.wei@foxmail.com>
2018-07-27 10:49:35 +0800

    新的状态管理系统sapceace

--- src/app.js
+++ src/app.js
@@ -1,16 +1,93 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const App = () => {
+import axios from "axios";
+const App = ({ state }) => {
   return (
     <div>
+      <h1>
+        {state.appName}
+      </h1>
       <p>React here!</p>
       <p>i like vue , but i'm prefer react</p>
 
       <p>
-        yep ! i'm restry, and i'm agree with your sense
+        yep ! i'm {state.user.name},{state.user.age} years old . and i'm agree with your sense. the submit form it's below
       </p>
+      <p>
+        {state.pizza.split(',').length}
+      </p>
+      <ol>
+        {state.pizza.split(',').map(a => <li>{a}</li>)}
+      </ol>
+
+      <PizzaForm space={state} />
     </div>
   );
 };
 export default App;
-ReactDOM.render(<App />, document.getElementById("app"));
+
+export const delay = (duration) => new Promise((resolve, reject) => setTimeout(resolve, duration));
+
+
+let state = { pizza: '' };
+
+const fetchPizza = async (data) => {
+  await delay(2000);
+
+  state.pizza += (data.name + ',');
+  return {
+    data: state,
+    error: ''
+  }
+}
+
+
+const handleSubmit = async ({ space, merge }, event) => {
+  event.preventDefault();
+
+  // merge does a shallow merge, allowing the assignment of multiple properties at once
+  merge({ saving: true }); // updates space immediately, triggers re-render
+
+  const { data, error } = await fetchPizza({ name: space.name });
+  if (error) return merge({ error: errorMsg, saving: false });
+
+  merge({
+    saving: false,
+    pizza: data.pizza // 'Pepperoni', hopefully
+  });
+};
+
+/*
+  handleReset is another custom action.
+  This one erases all properties on the space,
+  replacing them with only the specified ones.
+*/
+const handleReset = ({ replace }) => {
+  replace({
+    name: '',
+    pizzaLover: false
+  });
+};
+
+export const PizzaForm = ({ space }) => (
+  <form onSubmit={space(handleSubmit)}>
+    {/* ... some input elements */}
+    <label>Name</label>
+    <input
+      type="text"
+      value={space.name || ''}
+      onChange={space('name')} // Whenever user types, `space.name` is updated
+    />
+    <label>Do you like pizza?</label>
+    <input
+      type="checkbox"
+      checked={space.pizzaLover || false}
+      onChange={space('pizzaLover')} // Assigns true or false to `space.pizzaLover`
+    />
+    <div>
+      {space.saving && 'isSaving....'}
+    </div>
+    <p className="error">{space.errorMsg}</p>
+    <button disabled={space.saving} type="submit">Get Pizza</button>
+    <button disabled={space.saving} type="button" onClick={space(handleReset)}>Reset</button>
+  </form>
+);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
+import 'babel-polyfill';
 import App from "./app";
-
 import style from "./main.css";
+import ReactDOM from "react-dom";
+import React from "react";
 
 
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = (msg) => console.log(...arr,...msg);
-typeof window != 'undefined' && (window.iAmJavascriptES6 = iAmJavascriptES6);
-iAmJavascriptES6('im a silly entry point')
+import Space, { subscribe } from 'spaceace';
+
+const space = new Space({
+  appName: "SpaceAce demoe",
+  saving: false,
+  pizza: '',
+  user: { name: 'Jon', age: 18, pizzaLover: true }
+});
+
+subscribe(space, ({ newSpace, causedBy }) => {
+  console.log(`State updated by ${causedBy}`);
+
+
+  ReactDOM.render(<App state={newSpace} />,
+    document.getElementById("app"));
+
+  // Causes React to render
+  // setTimeout(() => {
+  //   newSpace({
+  //     user: {
+  //       name: 'Restry'
+  //     }
+  //   });
+  // }, 5000);
+
+});
+
+
+// Causes React to render
+setTimeout(() => {
+  space({ appName: "SpaceAce demo" });
+}, 1000);
+
+
